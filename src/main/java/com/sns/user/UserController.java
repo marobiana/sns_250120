889c4835abd0c29f171c15b0640ae1effc976041
test <test@test.com>
2025-05-26 18:45:29 +0900

    회원가입, 로그인 마크업

---
+++ src/main/java/com/sns/user/UserController.java
@@ -0,0 +1,27 @@
+package com.sns.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+    /**
+     * 회원가입 화면
+     * @return
+     */
+    @GetMapping("/sign-up-view")
+    public String signUpView() {
+        return "user/signUp";
+    }
+
+    /**
+     * 로그인 화면
+     * @return
+     */
+    @GetMapping("/sign-in-view")
+    public String signInView() {
+        return "user/signIn";
+    }
+}
